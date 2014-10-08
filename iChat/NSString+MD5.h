0610830e36640c3eeba7d3f8ca5e8fe2cbc2ad9f
Florian BUREL <florian.burel@mac.com>
2014-10-08 15:04:56 +0200

    Ajout de la categorie MD5

---
+++ iChat/NSString+MD5.h
@@ -0,0 +1,15 @@
+//
+//  NSString+MD5.h
+//  iChat
+//
+//  Created by Florian BUREL on 08/10/2014.
+//  Copyright (c) 2014 Florian Burel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (MD5)
+
+- (NSString *) md5;
+
+@end
